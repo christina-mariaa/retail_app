0aab4ccd3fc8f1d4bfecec23c0113c8e9f09a4cb
christina-mariaa <christina.mariaa@yandex.ru>
2025-04-01 22:06:31 +0300

    Исправлен url

--- products/models.py
+++ products/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
         latest_price = self.price_history.filter(end_date__isnull=True).order_by('-start_date').first()
         return latest_price.price if latest_price else None
 
+
 class PriceHistory(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='price_history')
     price = models.DecimalField(max_digits=10, decimal_places=2)
