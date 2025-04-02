6b4bca3c4c1b9914fc248da1ed5da6b3b1b628ff
christina-mariaa <christina.mariaa@yandex.ru>
2025-04-02 12:57:30 +0300

    исправлен сериализатор для Employee

--- people/serializers.py
+++ people/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Position, Employee, CounterAgent
+from locations.models import Location
 from locations.serializers import LocationSerializer
 
 
@@ -10,9 +11,13 @@ class PositionSerializer(serializers.ModelSerializer):
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
-    position = PositionSerializer()
-    location = LocationSerializer()
+    position_data = PositionSerializer(source='position', read_only=True)
+    location_data = LocationSerializer(source='location', read_only=True)
+
+    # Для записи — только id
+    position = serializers.PrimaryKeyRelatedField(queryset=Position.objects.all(), write_only=True)
+    location = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all(), write_only=True)
 
     class Meta:
         model = Employee
-        fields = ['id', 'name', 'surname', 'middle_name', 'position', 'location']
+        fields = ['id', 'name', 'surname', 'middle_name', 'position', 'location', 'position_data', 'location_data']
