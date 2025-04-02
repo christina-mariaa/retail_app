cfd277caa45935a0ccfc5b6a6a780627885d80e2
christina-mariaa <christina.mariaa@yandex.ru>
2025-04-02 12:40:04 +0300

    добавлено представление для Position

--- people/urls.py
+++ people/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet
+from .views import EmployeeViewSet, PositionViewSet
 
 
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet)
+router.register(r'employees-positions', PositionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
