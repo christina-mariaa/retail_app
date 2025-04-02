3b3dba8c582b3766bfb777c29c34d331af350ac4
Anastasiya <an4stasiyafedorova@yandex.ru>
2025-04-02 16:41:02 +0300

    Исправлено добавление, обновление статуса и сотрудников

--- sales/views.py
+++ sales/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.exceptions import ValidationError
 from .models import Order
 from .serializers import *
 
@@ -6,3 +9,10 @@ from .serializers import *
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
