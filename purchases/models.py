b1ffbd3cd3f504e998fa4133c97a8fa56a651783
Anastasiya <an4stasiyafedorova@yandex.ru>
2025-04-04 22:42:51 +0300

    Добавила вью функции для отчетов

--- purchases/models.py
+++ purchases/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from locations.models import Location
 from people.models import CounterAgent
 from products.models import Product
+from django.utils import timezone
 
 class Purchase(models.Model):
     supplier = models.ForeignKey(CounterAgent, on_delete=models.CASCADE, related_name='supplier_purchases')
     warehouse = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='warehouse_purchases')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Закупка {self.id} от {self.supplier.name}"
